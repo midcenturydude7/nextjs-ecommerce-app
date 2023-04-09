28d3ff2be6b6a5754a38442c6040d2b2a2a4367b
midCenturyDude7 <mgriffes@gmail.com>
2023-04-08 18:28:30 -0700

    Set up basic backend db schema with Sanity

--- sanity-ecommerce-app/schemas/product.js
+++ sanity-ecommerce-app/schemas/product.js
@@ -2,7 +2,7 @@ export default {
   name: 'product',
   title: 'Product',
   type: 'document',
-  field: [
+  fields: [
     {
       name: 'image',
       title: 'Image',
@@ -11,6 +11,30 @@ export default {
       options: {
         hotspot: true,
       },
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 90,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'details',
+      title: 'Details',
+      type: 'string',
     },
   ],
 }
