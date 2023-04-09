1b8b39c7822fb94614589be1aa5a5c3dd7b193b4
midCenturyDude7 <mgriffes@gmail.com>
2023-04-08 21:08:18 -0700

    Set up Sanity api call and loaded dynamic data to HeroBanner and Product components

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import Link from "next/link";
 
-function HeroBanner() {
-  return <div>HeroBanner</div>;
+import { urlFor } from "../lib/client";
+
+function HeroBanner({ heroBanner }) {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
+        <div>
+          <Link href={`/product/${heroBanner.id}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HeroBanner;
--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import Link from "next/link";
 
-function Product() {
-  return <div>Product</div>;
+import { urlFor } from "../lib/client";
+
+function Product({ product: { image, name, slug, price } }) {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            alt="Headphones"
+            width={250}
+            height={250}
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default Product;
